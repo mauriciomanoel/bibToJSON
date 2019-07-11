08dd6d78b34769c43501a31cf79c5e9f25ae0d98
Mauricio Manoel <mauriciomanoel@gmail.com>
2019-07-11 23:39:55 +0100

    update

--- parser.py
+++ parser.py
@@ -7,6 +7,17 @@ import os
 import re
 import unidecode
 
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key): byteify(value)
+                for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
 def main(argv = sys.argv):
 
 	argc = len(argv)
@@ -21,8 +32,9 @@ def main(argv = sys.argv):
 	bib = open(bib_filename,'a') # strip and replace extension
 	bib.write('[\n')
 	
-	for entrie in bib_database.entries:
-		r2b_write(entrie,bib)
+	print(bib_database.entries[0]['journal']).encode('ascii', 'ignore').decode('unicode_escape').strip()
+	#for entrie in bib_database.entries:
+	#	r2b_write(entrie,bib)
 
 	bib.write(']')
 	bib.close()	
@@ -33,11 +45,11 @@ def r2b_write(entrie,bib):
 	bib.write('\t\t"id": "' + id + '"')
 	if 'title' in entrie:
 		#title = entrie['title'].encode('utf-8').strip()
-		title = entrie['title'].replace("{\'{c}}", "c").encode('utf-8').strip()
+		title = entrie['title'].replace("{\_}", "_").replace("{\$}{\$}{\\backslash}mathcal{\{}PI{\}}{\$}{\$}", "PI").replace("{\\'{c}}", "c").replace("{\"u}", "u").replace("{\\&}", "&").replace("T{\\\"u}", "Tu").encode('utf-8').strip()
 		bib.write(',\n\t\t"title": "' + title.replace('"', '\\"') + '"')
 	if 'author' in entrie:
-		author = entrie['author'].encode('utf-8').strip()
-		bib.write(',\n\t\t"author": "' + author.replace('\n', ', ') + '"')		
+		author = entrie['author'].replace("{\\o}","o").replace("{\\v{s}}","s").replace("{\\u{g}}", "g").replace("{\\v{Z}}", "Z").replace("{\\l}", "l").replace("{\\i}","i").replace("{\\\"O}","O").replace("\\\"u}","u").replace("{\.{I}}","I").replace("{\\v{z}}","").replace("{\\v{e}}","").replace("{\\`A}", "A").replace("{\\ae}", "e").replace("{\\v{E}}","E").replace("{\\~{n}}", "n").replace("{\\`o}","o").replace("{\\aa}","a").replace("{\\c{c}}","c").replace("{\\'{c}}", "c").replace("{\\'u}", "u").replace("{\\'i}", "i").replace("{\\`e}","e").replace("{\\'o}", "o").replace("{\\'a}", "a").replace("{\\v{n}}","n").replace("{\\v{c}}","c").replace("{\\v{n}}", "n").replace("{\~a}", "a").replace("{\\`a}", "a").replace("{\\'A}", "A").replace("{\\'e}", "e").encode('utf-8').strip()
+		bib.write(',\n\t\t"author": "' + author.replace('\n', ', ') + '"')	
 	if 'publisher' in entrie:
 		bib.write(',\n\t\t"publisher": "' + entrie['publisher'] + '"')
 	if 'doi' in entrie:
@@ -45,8 +57,8 @@ def r2b_write(entrie,bib):
 	if 'isbn' in entrie:
 		bib.write(',\n\t\t"isbn": "' + entrie['isbn'] + '"')	
 	if 'abstract' in entrie:
-		abstract = entrie['abstract'].encode('utf-8').strip()
-		bib.write(',\n\t\t"abstract": "' + abstract.replace('"', '\\"').replace("`","'") + '"')
+		abstract = entrie['abstract'].replace("{","").replace("\\","").replace("}","").replace("{\\{}", "").replace("{\\}}", "").replace("{\\$}","").replace("Prot{\\e}g{\\e}","Protege").encode('utf-8').strip()
+		bib.write(',\n\t\t"abstract": "' + abstract.replace("{\$}{\$}{\\backslash}mathcal{\{}PI{\}}{\$}{\$}", "PI").replace("{\\&}", "&").replace('{\\e}',"e").replace("{\\ldots}", "").replace('"', '\\"').replace("`","'").replace("'","").replace("{\\thinspace}", "").replace("{\\%}", "%").replace("{\_}", "_") + '"')
 	if 'pages' in entrie:
 		bib.write(',\n\t\t"pages": "' + entrie['pages'] + '"')
 	if 'year' in entrie:
@@ -54,7 +66,7 @@ def r2b_write(entrie,bib):
 	if 'url_article' in entrie:
 		bib.write(',\n\t\t"url_article": "' + entrie['url_article'] + '"')
 	if 'journal' in entrie:
-		bib.write(',\n\t\t"journal": "' + entrie['journal'] + '"')
+		bib.write(',\n\t\t"journal": "' + entrie['journal'].replace("{\\&}", "&").replace("{\\'e}","e") + '"')
 	if 'month' in entrie:
 		bib.write(',\n\t\t"month": "' + entrie['month'] + '"')
 	if 'volume' in entrie:
